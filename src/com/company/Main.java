ebee1273deb355965ccd29fbf59e931984350307
Cristina <cristinaioana.c@gmail.co>
2019-12-19 20:36:46 +0200

    54

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int x = 2;
+        int y = 3;
+        int sum = x + y;
+        System.out.println(sum);
+
+        String suma=("Prima suma in Java este:" +(sum));
+
+        System.out.println(suma);
+
+    }
+}
