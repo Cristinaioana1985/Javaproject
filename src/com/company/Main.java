f7c3ffd8f06089a4181595a29cb7b6fa9eab45d3
Cristina <cristinaioana.c@gmail.co>
2020-01-06 23:46:53 +0200

    54

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,54 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        int x = 2;
-        float y = 3.14f;
-        float sum = x + y;
-               
-        System.out.println(sum);
+        System.out.println("Hello");
 
-        String suma=("Suma cifrelor x si y este:" +(sum));
+        System.out.println("Cristina");
 
-        System.out.println(suma);
+        int x = 20;
+        int y = 25;
+        int sum = x+y;
+        System.out.println ("Suma necunoscutelor x si y este:" +(sum));
+
+        float z = 10f;
+        float w = 3f;
+        float divison = z/w;
+        System.out.println ("Suma impartirii necunoscutelor z si w este:" +(divison));
+
+        int a = -5;
+        int b = 8;
+        int c = 6;
+        int multipl = b*c;
+        int operation1 = a + multipl;
+        System.out.println ("Suma operatiei -5+(8*6) este:" + (operation1 ));
+
+        int d = 55;
+        int e = 9;
+        float n = 9/100f;
+        float operation2 = (d+e)*n;
+        System.out.println ("Suma operatiei (55+9)%9 este:" + (operation2 ));
+
+        int f = 20;
+        int g = -3;
+        int h = 5;
+        float i = 8f;
+        int sum1 = g*h;
+        int sum2 = f+sum1;
+        float operation3= sum2/i;
+        System.out.println ("Suma operatiei (20+(-3*5))/8 este:" +(operation3));
+
+        int j = 15;
+        float k = 3f;
+        int l = 2;
+        float m = 8f;
+        float sumop1 = h+j;
+        float multipl1 = k*l;
+        float o = sumop1/multipl1;
+        float u = (m/100)*k;
+        float operation4= (o-u);
+        System.out.println ("Suma operatiei 5+15/3*2-8%3 este:" +(operation4));
 
 
-    }
+
+           }
 }
